c2894b1ad2a2946b972e5e9bbcf2e74b7286358b
root <root@DESKTOP-Q5A5F5T.localdomain>
2021-10-26 10:06:35 -0500

    v1

--- deleteimages.sh
+++ deleteimages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v 
 file="./imagenesnoborrar.txt"
 #Filtrado de imagenes de docker por nombre de la imagen (.Repository) y por dias de creadas 
-docker images --filter=reference='*' --format "{{.Repository}} {{.ID}} {{.CreatedSince}}" | grep -i 'days' | awk '{ print $1 }' > images
+docker images --filter=reference='*' --format "{{.Repository}} {{.ID}} {{.CreatedSince}}" | grep -i 'days' | awk '{ print $1 }' > images.txt
 f="true" #inicializacion de la variale f para comparar si existe
 
 #mostrar los archivos
@@ -15,29 +15,27 @@ do
    a="$u"
 
         while read LINE; do
-        b="$LINE" #lectura de la linea del archivo file
-        echo "Archivo imagenesNoborrar.txt: $LINE "
+           b="$LINE" #lectura de la linea del archivo file
+           echo "Archivo imagenesNoborrar.txt: $LINE "
 
-	if [ "$a" == "$b" ]; then
-	# echo "**NO BORRAR la imagen *** $a = $b "
-         f=1 
-    	else
-         # echo "------SI BORRAR, $a != $b"
-	  c=0
-	fi
+	   if [ "$a" == "$b" ]; then
+	      # echo "**NO BORRAR la imagen *** $a = $b "
+            f=1 
+    	   else
+            # echo "------SI BORRAR, $a != $b"
+	    c=0
+	   fi
 	done < $file;
-# echo "aqui paso la primera lectura de todos"
-# echo "f = $f "
-# echo "c= $c"
-if [[ $f == "true" && $c == "0" ]]; then
+
+ if [[ $f == "true" && $c == "0" ]]; then
 	echo "la imagen $a  SE PUEDE BORRAR"
 	echo "docker rmi $a"
-docker rmi $a	
-elif [[ $f == "1" && $c == "0" ]]; then
+        docker rmi $a	
+ elif [[ $f == "1" && $c == "0" ]]; then
 	        echo "la imagen $a esta en el archivo de no borrar y NO SEPUED BORRAR"
-elif [[ $f == "true" && $c == "1" ]]; then
+ elif [[ $f == "true" && $c == "1" ]]; then
         echo "la imagen $a esta en el archivo de no borrar NO SE PUEDE BORRAR"	
-fi
+ fi
 done
 
 
